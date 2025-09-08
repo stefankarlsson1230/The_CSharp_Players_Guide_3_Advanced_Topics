2d244f654fb02e66d2a89ff0f0120561fbf3b17f
stefankarlsson1230@gmail.com <stefankarlsson1230@gmail.com>
2025-09-08 15:39:08 +0200

    Finished Project 34_3

---
+++ Project_34_3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Optional Greeting - Default param "Hello".
+
+Console.WriteLine(Greeting("I don't use default greetings!"));
+Console.WriteLine(Greeting());
+
+
+
+// Local functions
+string Greeting(string greeting = "Hello") => greeting;
+
