efa9916ca1e9718bb5db33a7ad4dbeeca145560b
stefankarlsson1230@gmail.com <stefankarlsson1230@gmail.com>
2025-09-17 09:32:53 +0200

    Finished Project_41_2

---
+++ Project_41_2/Program.cs
@@ -0,0 +1,52 @@
+﻿BlockCoordinate block = new(5, 8); 
+
+Console.WriteLine($"{block}  Row: {block[0]}  Column: {block[1]}");
+
+
+
+
+public record BlockCoordinate(int Row, int Column)
+{
+    // Operators
+    public static BlockCoordinate operator +(BlockCoordinate a, BlockOffset b) => new BlockCoordinate(a.Row + b.RowOffset, a.Column + b.ColumnOffset);
+    public static BlockCoordinate operator +(BlockOffset a, BlockCoordinate b) => new BlockCoordinate(a.RowOffset + b.Row, a.ColumnOffset + b.Column);
+
+    public static BlockCoordinate operator +(BlockCoordinate a, Direction b)
+    {
+        return a + b switch
+        {
+            Direction.North => new BlockOffset(-1, 0),
+            Direction.East => new BlockOffset(0, 1),
+            Direction.South => new BlockOffset(1, 0),
+            Direction.West => new BlockOffset(0, -1),
+        };
+    }
+    public static BlockCoordinate operator +(Direction a, BlockCoordinate b) => b + a;
+
+    // Indexer
+    public int this[int index]
+    {
+        get 
+        {
+            if (index == 0) return Row;
+            else return Column;
+        }
+    }
+
+
+    // Methods
+    public override string ToString()
+    {
+        return $"({Row}, {Column})";
+    }
+}
+
+public record BlockOffset(int RowOffset, int ColumnOffset)
+{
+    public override string ToString()
+    {
+        return $"({RowOffset}, {ColumnOffset})";
+    }
+}
+
+public enum Direction { North, East, South, West }
